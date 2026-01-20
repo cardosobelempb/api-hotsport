054f44ed9ae64351c21ff7a4dbe5a66552113ef5
cardosobelempb <claudio.c.lima@hotmail.com>
2026-01-20 08:31:46 -0300

    update readme.md

--- src/api/hotspot.ts
+++ src/api/hotspot.ts
@@ -1,18 +1,19 @@
-import { RouterOSAPI } from "node-routeros";
-import { routerConfig } from "../config/router.config";
-import { appConfig } from "../config/appConfig";
+import { RouterOSAPI } from 'node-routeros'
+
+import { appConfig } from '../config/appConfig'
+import { routerConfig } from '../config/router.config'
 
 const API = new RouterOSAPI({
   host: routerConfig.host,
   user: routerConfig.user,
   password: routerConfig.password,
-});
+})
 
 try {
-  await API.connect();
+  await API.connect()
 
   // Adicionar usuário Hotspot
-  await API.write("/ip/hotspot/user/add", [
+  await API.write('/ip/hotspot/user/add', [
     `=server=${appConfig.mikhmon}`,
     `=name=${name}`,
     `=password=${password}`,
@@ -22,13 +23,13 @@ try {
     `=limit-uptime=${timelimit}`,
     `=limit-bytes-total=${datalimit}`,
     `=comment=${userComment}`,
-  ]);
+  ])
 
   // Buscar usuário adicionado
-  const result = await API.write("/ip/hotspot/user/print", [`?name=${name}`]);
-  const response = await API.read(); // lê o resultado do comando
+  const result = await API.write('/ip/hotspot/user/print', [`?name=${name}`])
+  const response = await API.read() // lê o resultado do comando
 
-  console.log(response); // aqui você terá os dados do usuário
+  console.log(response) // aqui você terá os dados do usuário
 } finally {
-  API.close();
+  API.close()
 }
